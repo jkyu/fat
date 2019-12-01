Fixed error in the normalization of the fluorescence signal that was caused by the original hard-coded two-state problem. Small fix for the plotting of the steady state fluorescence plot.

2-analysis/fluorescence/compute-fluorescence.py
@@ -144,8 +144,6 @@ def approx_fluorescence(egrid, tgrid, en_gap, intens):
             if dE < 0.1:
                 fl_grid[ind, t] += flin
 
-    fl_grid = fl_grid / np.max(fl_grid)
-
     return fl_grid
 
 def convolve(egrid, tgrid, en_gap, tsteps, intens, sigE=0.15, sigT=65):
@@ -159,8 +157,6 @@ def convolve(egrid, tgrid, en_gap, tsteps, intens, sigE=0.15, sigT=65):
         for idx2, cT in enumerate(conv_T):
             conv_E = cT * np.exp( -0.5 * ((egrid - e0)/sigE)**2 ) * (1./(sigE*np.sqrt(2.*np.pi))) 
             fl_grid[idx2,:] += conv_E
-
-    fl_grid = fl_grid / np.max(fl_grid)
 
     return fl_grid
 
@@ -195,6 +191,6 @@ if __name__=='__main__':
     fmsinfo = pickle.load(open(datadir+'/fmsinfo.pickle', 'rb'))
     ics = fmsinfo['ics']
     npoints = 201
-    tgrid  = np.linspace(-90., 510., npoints)
-    wgrid = np.linspace(340, 1140, npoints)
+    tgrid  = np.linspace(-90., 260., npoints)
+    wgrid = np.linspace(100, 1140, npoints)
     compute_fluorescence(ics, tgrid, wgrid, datadir)

2-analysis/fluorescence/plot-time-resolved.py
@@ -216,7 +216,7 @@ def run():
 
     for i in range(2): 
         plt.plot(fl_data['tgrid'], slices[i], linewidth=2.0, color=colors[1], linestyle=styles[i])
-        plt.errorbar(fl_data['tgrid'][(i*5)::10], slices[i][(i*5)::10], yerr=error_slices[i][(i*5)::10], color=colors[1], linewidth=2.0, capsize=2.0, elinewidth=0.8, ecolor=error_colors[i], linestyle=styles[i], label='AIMS, %d nm, $\\tau=$%d fs' %(np.array([650, 800])[i], taus[i+2]))
+        plt.errorbar(fl_data['tgrid'][(i*5)::10], slices[i][(i*5)::10], yerr=error_slices[i][(i*5)::10], color=colors[1], linewidth=0, capsize=2.0, elinewidth=0.8, ecolor=error_colors[i], linestyle=styles[i], label='AIMS, %d nm, $\\tau=$%d fs' %(np.array([650, 800])[i], taus[i+2]))
     plt.plot(expt_t+tshift, expt_fl_650, label='Expt, 650 nm, $\\tau=$%d fs' %taus[0], linewidth=2.0, color=colors[0], linestyle=styles[0])
     plt.plot(expt_t+tshift, expt_fl_800, label='Expt, 800 nm, $\\tau=$%d fs' %taus[1], linewidth=2.0, color=colors[0], linestyle=styles[1])
 