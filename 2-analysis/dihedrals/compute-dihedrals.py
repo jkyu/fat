fc48862fa31c6a53f0bc1564561206e823c21fc4
jkyu <jkyu@stanford.edu>
2019-12-01 23:51:46 -0800

    Cleaned up dihedral plotting code, but still need to work on generalizing it.

--- 2-analysis/dihedrals/compute-dihedrals.py
+++ 2-analysis/dihedrals/compute-dihedrals.py
@@ -103,7 +103,6 @@ def compute_dihedrals(ics, tgrid, datadir, nstates, dihedral_index, do_avg=False
                     dihes[i] = dihes[i] - 360
                 elif (dihes[i] - dihes[i-1]) < -300:
                     dihes[i] = dihes[i] + 360
-            # dihes = [ x - 360. if x>100 else x for x in dihes ]
             interp_dihes = interpolate(tgrid, tsteps, dihes, do_avg=do_avg)
             dihes_dict[dihe_name] = interp_dihes
 
@@ -114,63 +113,6 @@ def compute_dihedrals(ics, tgrid, datadir, nstates, dihedral_index, do_avg=False
         interp_zeroed[tbf_key] = zeroed_dict
         interp_dihedrals[tbf_key] = dihes_dict
 
-    # if do_avg:
-    #     print('Averaging dihedral angles across excited state TBFs.')
-    #     avg_dihedrals_ex = {}
-    #     error_ex = {}
-    #     for dihe_name in dihedral_names:
-    #         aggregated_dihedrals = np.zeros((len(ex_keys), len(tgrid)))
-    #         for i, tbf_key in enumerate(ex_keys):
-    #             aggregated_dihedrals[i,:] = interp_dihedrals[tbf_key][dihe_name]
-    #         avg_dihedrals_ex[dihe_name] = np.mean(aggregated_dihedrals, axis=0)
-    #         
-    #         # Bootstrap sampling error
-    #         # At each time step, sample the dihedrals for each excited state TBF
-    #         # with one of the TBFs excluded. Do this 1000 times and take the 
-    #         # average of each of these n-1 angle lists. The standard deviation
-    #         # measures the sampling error via bootstrapping.
-    #         error = []
-    #         for k in range(len(tgrid)):
-    #             sample_inds = np.arange(len(ex_keys))
-    #             resample = [ [aggregated_dihedrals[x,k] for x in np.random.choice(sample_inds, size=(len(sample_inds)), replace=True)] for _ in range(1000) ]
-    #             resampled_means = np.mean(resample, axis=1)
-    #             std = np.std(resampled_means)
-    #             error.append(std)
-
-    #         error_ex[dihe_name] = np.array(error)
-
-    # print('Separating cis and trans TBFs.')
-    # cis_keys = []
-    # trans_keys = []
-    # for tbf_key in gs_keys:
-    #     idx = np.max(np.argwhere(np.isfinite(interp_dihedrals[tbf_key]['CNNC'])))
-    #     final_angle = interp_dihedrals[tbf_key]['CNNC'][idx]
-    #     if final_angle > 270. or final_angle < 90.:
-    #         cis_keys.append(tbf_key)
-    #     else:
-    #         trans_keys.append(tbf_key)
-
-    # if do_avg: 
-    #     print('Averaging dihedral angles across cis ground state TBFs.')
-    #     avg_dihedrals_gs = {}
-    #     error_gs = {}
-    #     for dihe_name in dihedral_names:
-    #         aggregated_dihedrals = np.zeros((len(cis_keys), len(tgrid)))
-    #         for i, tbf_key in enumerate(cis_keys):
-    #             aggregated_dihedrals[i,:] = interp_zeroed[tbf_key][dihe_name]
-    #         avg_dihedrals_gs[dihe_name] = np.mean(aggregated_dihedrals, axis=0)
-    #         
-    #         # Bootstrap sampling error
-    #         error = []
-    #         for k in range(len(tgrid)):
-    #             sample_inds = np.arange(len(cis_keys))
-    #             resample = [ [aggregated_dihedrals[x,k] for x in np.random.choice(sample_inds, size=(len(sample_inds)), replace=True)] for _ in range(1000) ]
-    #             resampled_means = np.mean(resample, axis=1)
-    #             std = np.std(resampled_means)
-    #             error.append(std)
-
-    #         error_gs[dihe_name] = np.array(error)
-
     # Cache data
     data2 = {}
     data2['dihedral_names'] = dihedral_names
@@ -178,24 +120,11 @@ def compute_dihedrals(ics, tgrid, datadir, nstates, dihedral_index, do_avg=False
     data2['populations'] = interp_populations
     data2['state_ids'] = state_ids
     data2['tbf_keys'] = [x for x in state_ids.keys()]
-    # if do_avg: 
-    #     data2['avg_dihedrals_ex'] = avg_dihedrals_ex
-    #     data2['avg_dihedrals_gs'] = avg_dihedrals_gs
-    #     data2['error_ex'] = error_ex
-    #     data2['error_gs'] = error_gs
     data2['tgrid'] = tgrid
-    # data2['ex_keys'] = ex_keys
-    # data2['gs_keys'] = gs_keys
-    # data2['cis_keys'] = cis_keys
-    # data2['trans_keys'] = trans_keys
 
     print('Dumping interpolated amplitudes to dihedrals.pickle')
     if not os.path.isdir('./data/'):
         os.mkdir('./data/')
-    # if do_avg:
-    #     with open('./data/dihedrals-avgs.pickle', 'wb') as handle:
-    #         pickle.dump(data2, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    # else:
     with open('./data/dihedrals.pickle', 'wb') as handle:
         pickle.dump(data2, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -206,9 +135,9 @@ dihedral angles. Pass this dictionary into compute_dihedrals()
 '''
 print('Indexing dihedral angles.')
 dihedral_index = {}
-dihedral_index['CNNC'] = [18, 13, 0, 5]
+dihedral_index['HCCH'] = [0, 4, 5, 3]
 
-tgrid = np.arange(0, 1500, 5)
+tgrid = np.arange(0, 250, 5)
 datadir = '../../1-collect-data/data/'
 fmsinfo = pickle.load(open(datadir+'/fmsinfo.pickle', 'rb'))
 ics = fmsinfo['ics']
