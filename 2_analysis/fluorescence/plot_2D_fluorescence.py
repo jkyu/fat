220ce6293dfc8887fbe91e38c3ce6797c0d89ef8
jkyu <jkyu@stanford.edu>
2020-01-11 18:21:44 -0800

    Updated fluorescence plotter to reduce grid size required by the initial fluorescence computation.

--- 2_analysis/fluorescence/plot_2D_fluorescence.py
+++ 2_analysis/fluorescence/plot_2D_fluorescence.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.interpolate
 import os
 import math
 import sys
@@ -7,6 +8,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.colors as cl
+from matplotlib import rcParams
 '''
 This plots the 2D fluorescence spectrum from data we computed in
 compute-fluorescence.py that should have been dumped to
@@ -15,59 +17,73 @@ This uses a fluorescence shift that should be computed in the
 output of plot-time-resolved.py and saved to ./data/fl_shift.txt
 '''
 
-def plot_fluorescence(fl_data, fl_shift, wgrid, tgrid, figname='2D-fluorescence', lineout=False, lineout_wls=None):
+def interpolate_fluorescence(tgrid, wgrid, fgrid, ngrid=1000):
+    ''' 
+    Use cubic interpolation to place the computed fluorescence grid on a finer mesh.
+    ngrid is the grid size for the new interpolated grid.
+    '''
+    # Unravel the fluorescence grid 
+    ts = []
+    ws = []
+    fs = []
+    for i, t in enumerate(tgrid):
+        for j, w in enumerate(wgrid):
+            ts.append(t)
+            ws.append(w)
+            fs.append(fgrid[i,j])
+    # Expand the time and wavelength grids
+    tgrid2 = np.linspace(tgrid[0], tgrid[-1], ngrid)
+    wgrid2 = np.linspace(wgrid[0], wgrid[-1], ngrid)
+    # Place the expanded grids on a mesh.
+    tgrid3, wgrid3 = np.meshgrid(tgrid2, wgrid2)
+    # Interpolate the fluorescence grid onto the mesh.
+    fgrid3 = scipy.interpolate.griddata( (ts, ws), fs, (tgrid3, wgrid3), method='cubic', fill_value=0)
+    # Normalize the interpolated grid.
+    fgrid3 = fgrid3 / np.max(fgrid3)
+
+    return tgrid3, wgrid3, fgrid3
+
+def plot_fluorescence(fgrid, wgrid, tgrid, figname='2D_fluorescence', fl_shift=0, lineout_wls=[]):
     '''
     Here, we think about time as x and energy as y. It turns out that in
     the fluorescence 2D array, each row corresponds to a specific time 
     value, so the first index (rows) gives the time and the second 
-    (column) index is energy. This means that time is on the y-axis and 
-    energy is on the x-axis if we use the array as a plot. So we need
-    to transpose this array. 
+    (column) index is energy. The energy grid is interchangeable with the
+    wavelength grid. 
+    First, we place the fluorescence grid on a finer mesh and then we plot
+    the 2D spectrum as a contour plot. Then we plot it with a custom 
+    color scheme. 
     '''
-    egrid = np.array([1240./x for x in wgrid])
-    ngrid = len(tgrid)
 
-    t0 = np.argmin(np.abs(0 - tgrid))
-    xticks = np.arange(t0, ngrid, ngrid//5)
-    xlabels = ['%d' %int(k) for k in tgrid[xticks]]
-    yticks = np.arange(0, ngrid, ngrid//5)
-    ylabels = ['%d' %int(k) for k in wgrid[yticks]]
+    print('Interpolating the fluorescence grid.')
+    tgrid3, wgrid3, fgrid3 = interpolate_fluorescence(tgrid, wgrid, fgrid)
 
-    print('Normalizing the fluorescence signal')
-    fl2 = fl.T
-    fl2 =  fl2 / np.max(fl2)
-
-    ''' 
-    Custom color map taken by extracting RGB values from Schmidt 2005 figure 1
-    in order to match the scale and colors. 
-    '''
-    colors = [(101, 0, 145), (37, 0, 250), (0, 61, 255), (0, 162, 255), (0, 246, 255), (0, 255, 175), (1, 255, 0), (187, 255, 0), (255, 234, 0), (255, 162, 0), (255, 88, 0), (255, 13, 0) ] 
-    colors = [ (x[0]/255., x[1]/255., x[2]/255.) for x in colors]
-    
-    cmap_name = 'special'
-    cm = cl.LinearSegmentedColormap.from_list(cmap_name, colors, N=len(colors))
-
+    print('Plotting the fluorescence grid.')
     fig = plt.figure(facecolor='white', figsize=(6,5))
     labelsize = 16
     ticksize = 14
-    legendsize = 12
+    plt.rc('xtick',labelsize=ticksize)
+    plt.rc('ytick',labelsize=ticksize)
 
-    plt.imshow(fl2, origin='lower', cmap=cm)
+    cm = plt.get_cmap('rainbow')
+    # cm = plt.get_cmap('jet')
+    ngrid_cm = 51
+    levels = np.linspace(np.min(fgrid3), np.max(fgrid3), ngrid_cm)
+    fl_im = plt.contourf(tgrid3, wgrid3, fgrid3, cmap=cm, levels=levels, vmin=np.min(fgrid3), vmax=np.max(fgrid3))
+    cb = fig.colorbar(fl_im, format='%.1f')
+    cb.set_ticks( [ x for x in np.arange( np.min(fgrid3), np.max(fgrid3)+0.1, 0.1) ] )
+    # cb.set_label('Fluorescence Intensity', rotation=270, fontsize=labelsize)
 
-    # Plot line outs -- if you don't have single wavelength signal to highlight,
-    # you can comment this block out
-    if lineout:
+    # Plot lineouts if requested by giving a list of lineout wavelengths
+    if len(lineout_wls) > 0:
         for wl in lineout_wls:
-            lineout_wl = np.argmin(np.abs(egrid - (1240./wl + fl_shift)))
-            plt.plot(np.arange(ngrid), [lineout_wls]*len(tgrid), linewidth=2.0, linestyle='-', color='silver')
+            plt.plot([tgrid[0], tgrid[-1]], [wl]*2, linewidth=1.5, linestyle='--', color='silver')
 
-    plt.xticks(xticks, xlabels, fontsize=ticksize)
-    plt.yticks(yticks, ylabels, fontsize=ticksize)
     plt.xlabel('Time [fs]',fontsize=labelsize)
     plt.ylabel('Wavelength [nm]',fontsize=labelsize)
-    plt.colorbar()
-    # fig.autofmt_xdate()
     plt.tight_layout()
+
+    print('Saving the fluorescence figure to ./figures/%s.pdf' %figname)
     if not os.path.isdir('./figures/'):
         os.mkdir('./figures/')
     plt.savefig('./figures/%s.pdf' %figname, dpi=300)
@@ -80,6 +96,6 @@ fl_data = pickle.load(open('./data/fluorescence.pickle', 'rb'))
 wgrid = fl_data['wgrid']
 tgrid = fl_data['tgrid']
 fl    = fl_data['fluorescence']
-lineout_wls = [650, 850]
 figname = '2D_fluorescence'
-plot_fluorescence(fl, fl_shift, wgrid, tgrid, figname=figname, lineout=True, lineout_wls=lineout_wls)
+lineout_wls = [400, 600]
+plot_fluorescence(fl, wgrid, tgrid, figname=figname, fl_shift=fl_shift, lineout_wls=lineout_wls)
